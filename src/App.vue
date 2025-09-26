1db70f37b256f411a527ee54f7d4055bdbe7cf96
MukaiTaiyo <luckdoor.jp@gmail.com>
2025-09-27 06:40:48 +0900

    Initialize Vue 3 project with Vite, ESLint, Prettier, and TypeScript support. Add essential configuration files, project structure, and basic components for a TODO application.

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import MainTodo from '@/components/MainTodo.vue';
+import TheHeader from '@/components/TheHeader.vue';
+import TheFooter from '@/components/TheFooter.vue';
+</script>
+
+<template>
+  <div class="wrap">
+    <TheHeader />
+    <main class="main">
+      <MainTodo />
+    </main>
+    <TheFooter />
+  </div>
+</template>
+
+<style>
+.wrap {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  min-height: 90vh;
+  padding: 0;
+  margin: 0;
+  font-family: Arial, sans-serif;
+  background-color: #f8f8f8;
+}
+
+.main {
+  flex: 1;
+  padding: 16px;
+}
+</style>
---
+++ src/components/MainTodo.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const todo = ref('');
+
+const addTodo = () => {
+  console.log(todo.value);
+};
+</script>
+
+<template>
+  <div class="box_input">
+    <input type="text" class="todo_input" v-model="todo" placeholder="+ TODOを入力" />
+    {{ todo }}
+    <button class="btn" @click="addTodo">追加</button>
+  </div>
+</template>
+
+<style scoped>
+.todo_input {
+  width: 250px;
+  padding: 6px 8px;
+  margin-right: 8px;
+  font-size: 18px;
+  border: 1px solid #aaa;
+  border-radius: 6px;
+}
+
+.btn {
+  position: relative;
+  padding: 6px 8px;
+  font-size: 14px;
+  color: #fff;
+  text-align: center;
+  background-color: #03a9f4;
+  border: 1px solid #eee;
+  border-radius: 6px;
+}
+
+.btn:active {
+  top: 1px;
+}
+</style>
